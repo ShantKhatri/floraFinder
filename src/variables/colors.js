bf71fd4eeed63bb207448d8537e058e142748b13
Prashantkumar Khatri <prashantkhatri202@gmail.com>
2023-12-28 17:17:19 +0530

    global color variable set up

---
+++ src/variables/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  primaryBackground: "#ECEDE1",
+  secondaryBackground: "#CACFC0",
+  modalBackground: "#ECEBDF",
+  primaryButton: "#5F7A5D",
+  secondaryButton: "#ECEDE1",
+};
+
+export default colors;
