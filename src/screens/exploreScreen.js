21aee53e203cd308cf576be91bc0c560123a1b5d
Prashantkumar Khatri <prashantkhatri202@gmail.com>
2023-12-29 01:45:58 +0530

    Explore Screen - Category and related to category results list

--- src/screens/exploreScreen.js
+++ src/screens/exploreScreen.js
@@ -1,34 +1,222 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  FlatList,
+} from "react-native";
+import colors from "../variables/colors";
+import PlantCard from "../components/plantCard";
+import fetch from "node-fetch";
+import { AntDesign } from "@expo/vector-icons";
 
 const ExploreScreen = ({ navigation }) => {
+  const [textResult, setTextResult] = useState([]);
+  const [pageNo, setPageNo] = useState(1);
+  const [category, setCategory] = useState("plants");
   // Define an array of species or classifications
   const species = [
-    { id: 1, name: "Species 1" },
-    { id: 2, name: "Species 2" },
-    { id: 3, name: "Species 3" },
+    { id: 1, name: "plants" },
+    { id: 3, name: "species" },
     // Add more species as needed
   ];
 
-  // Function to handle species selection
-  const handleSpeciesSelection = (speciesId) => {
-    // Navigate to the screen showing plants with the selected species
-    navigation.navigate("PlantsScreen", { speciesId });
+  const renderCategory = ({ item }) => {
+    return (
+      <TouchableOpacity
+        style={styles.category}
+        onPress={() => {
+          setPageNo(1);
+          setCategory(item.name);
+          fetchCategory(pageNo, category);
+        }}
+      >
+        <Image
+          source={{
+            uri: "https://storage.googleapis.com/powop-assets/neotropikey/tapura_peruviana_1_fullsize.jpg",
+          }}
+          style={styles.categoryImage}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            backgroundColor: colors.secondaryBackground,
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            borderBottomLeftRadius: 8,
+            borderBottomRightRadius: 8,
+          }}
+        >
+          <Text style={styles.categoryName}>{item.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
   };
 
-  return (
-    <View>
-      <Text>Explore Screen</Text>
-      {species.map((speciesItem) => (
+  const renderResult = ({ item }) => {
+    return (
+      <View
+        style={{
+          // flex: 1,
+          backgroundColor: colors.primaryBackground,
+          width: "45%",
+          marginHorizontal: 10,
+          alignSelf: "center",
+        }}
+      >
+        <PlantCard key={item.id + item.name} plant={item} />
+      </View>
+    );
+  };
+
+  const fetchCategory = async (page, category) => {
+    try {
+      const response = await fetch(
+        `https://trefle.io/api/v1/${category}?token=6-zgTrpjdpJK-7MqVo_iXczQRpdIq_hmEIDfdhTUxlg&page=${page}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error fetching data: ${response.statusText}`);
+      }
+
+      const json = await response.json();
+      const newData = json.data;
+
+      // Assuming textResult is a state variable
+      setTextResult(newData);
+
+      // Log the current state of textResult
+      // console.log(textResult);
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategory(pageNo, category);
+  }, []);
+
+  const pageNavigation = () => {
+    return (
+      <View style={styles.pageNavigation}>
         <TouchableOpacity
-          key={speciesItem.id}
-          onPress={() => handleSpeciesSelection(speciesItem.id)}
+          onPress={() => {
+            setPageNo(pageNo - 1);
+            fetchCategory(pageNo, category);
+          }}
+          style={{
+            backgroundColor: colors.primaryButton,
+            height: 40,
+            width: 80,
+            justifyContent: "space-between",
+            paddingHorizontal: 10,
+            alignItems: "center",
+            borderRadius: 8,
+            flexDirection: "row",
+          }}
         >
-          <Text>{speciesItem.name}</Text>
+          <AntDesign
+            name="leftcircle"
+            size={24}
+            color={colors.secondaryButton}
+          />
+          <Text style={{ fontSize: 16 }}>Prev</Text>
         </TouchableOpacity>
-      ))}
+        <TouchableOpacity
+          onPress={() => {
+            setPageNo(pageNo + 1);
+            fetchCategory(pageNo, category);
+          }}
+          style={{
+            backgroundColor: colors.primaryButton,
+            height: 40,
+            width: 80,
+            justifyContent: "space-between",
+            paddingHorizontal: 10,
+            alignItems: "center",
+            borderRadius: 8,
+            flexDirection: "row",
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>Next</Text>
+          <AntDesign
+            name="rightcircle"
+            size={24}
+            color={colors.secondaryButton}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  const fetchResult = () => {
+    return (
+      <View style={{ alignItems: "center", flex: 1 }}>
+        <FlatList
+          data={textResult}
+          renderItem={renderResult}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+          ListFooterComponent={pageNavigation}
+        />
+      </View>
+      // textResult.map((item) => <PlantCard key={item.id} plant={item} />)
+    );
+  };
+  return (
+    <View style={{ flex: 1, backgroundColor: colors.primaryBackground }}>
+      {/* {fetchCategory(pageNo, category)} */}
+      <View style={styles.categoryContainer}>
+        <FlatList
+          data={species}
+          keyExtractor={(item) => item.id + item.name}
+          renderItem={renderCategory}
+          horizontal
+        />
+      </View>
+      <View style={styles.exploreContainer}>{fetchResult()}</View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  categoryContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    // marginHorizontal: 20,
+    marginTop: 20,
+  },
+  category: {
+    // backgroundColor: "#fff",
+    borderRadius: 10,
+    alignItems: "center",
+    padding: 10,
+    width: 180,
+    height: 200,
+  },
+  categoryImage: {
+    width: "100%",
+    height: "80%",
+    borderTopRightRadius: 8,
+    borderTopLeftRadius: 8,
+  },
+  categoryName: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  exploreContainer: {
+    flex: 1,
+  },
+  pageNavigation: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+});
 
 export default ExploreScreen;
