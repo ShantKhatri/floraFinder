567f3e2c29508ee52e1d350dcab8cdd049855b4c
Prashantkumar Khatri <prashantkhatri202@gmail.com>
2024-01-03 00:40:23 +0530

    packages version update and setup for eas build

--- App.js
+++ App.js
@@ -5,9 +5,21 @@ import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import BottomTab from "./src/navigation/bottomTab/BottomTab";
 import LoginScreen from "./src/screens/account/login/login";
 import { FavouritesProvider } from "./src/contexts/FavouritesContext";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 // import { Settings } from "react-native";
 
 const App = () => {
+  const StoreProfilePicture = async (image) => {
+    try {
+      const jsonValue = JSON.stringify(image);
+      await AsyncStorage.setItem("profilePicture", jsonValue);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  StoreProfilePicture(
+    "https://i.pinimg.com/564x/5e/f1/4e/5ef14efc02c7dd1da2c1d02731b6bf8f.jpg"
+  );
   return (
     <FavouritesProvider>
       <NavigationContainer>
