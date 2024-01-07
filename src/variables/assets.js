eac326212b92d08b2ee61f08c7e7bdd7bfb07e3f
Prashantkumar Khatri <prashantkhatri202@gmail.com>
2024-01-07 17:55:01 +0530

    unwanted assets removed

--- src/variables/assets.js
+++
@@ -1,4 +0,0 @@
-export const speciesCategory = require("../../assets/speciesCategory.png");
-export const plantsCategory = require("../../assets/plantsCategory.png");
-export const lens = require("../../assets/lens.png");
-export const cryingPlant = require("../../assets/cryingPlant.png");
