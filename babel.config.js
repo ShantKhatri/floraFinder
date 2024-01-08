6677a228d9221a55eb353e327e71596d292ec7a7
Prashantkumar Khatri <prashantkhatri202@gmail.com>
2024-01-08 16:10:25 +0530

    eas build issue fix: module resolver issue

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,15 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["transform-inline-environment-variables"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [".tsx", ".ts", ".js", ".json"],
+        },
+      ],
+      "react-native-reanimated/plugin",
+      "transform-inline-environment-variables",
+    ],
   };
 };
