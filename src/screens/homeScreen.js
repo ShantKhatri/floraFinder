93cc092655f1146ae306c00c806588d572e1727a
Prashantkumar Khatri <prashantkhatri202@gmail.com>
2024-01-09 19:26:49 +0530

    Activity loader fix for homeScreen

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
 import {
   View,
-  Text,
-  Image,
   TouchableOpacity,
   StyleSheet,
   ImageBackground,
-  ScrollView,
   SafeAreaView,
 } from "react-native";
-import SearchPlantScreen from "./searchPlant";
 import PlantOfTheDayCard from "../components/plantOfTheDayCard";
 import plantsOfTheDayData from "../services/plantsOfTheDay";
 import colors from "../variables/colors";
@@ -19,7 +15,6 @@ const HomeScreen = () => {
   const [plantsOfTheDay, setPlantsOfTheDay] = useState([]);
   const [selectedPlant, setSelectedPlant] = useState(1);
   const [loadingComplete, setLoadingComplete] = useState(false);
-  const [loading, setLoading] = useState(false);
 
   // console.log("DATA");
 
@@ -28,34 +23,28 @@ const HomeScreen = () => {
       const data = await plantsOfTheDayData;
       console.log("DATA", data);
       setPlantsOfTheDay(data);
-      setLoading(false);
+      setLoadingComplete(true);
     } catch (error) {
       console.error("Error fetching plants of the day:", error);
     }
   };
 
   useEffect(() => {
-    setLoading(true);
+    setLoadingComplete(false);
     console.log("USE EFFECT");
     fetchPlantsOfTheDay();
   }, []);
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
       await new Promise((resolve) => setTimeout(resolve, 3000));
-
       setLoadingComplete(true);
-      setLoading(false);
     };
 
     fetchData();
   }, []);
 
-  // const image1 = plantsOfTheDay[1].image_url;
-
   const CircleWithSections = (item, index) => {
-    // console.log(plantsOfTheDay[0].scientific_name);
     return (
       <TouchableOpacity
         style={{
@@ -93,16 +82,15 @@ const HomeScreen = () => {
         backgroundColor: colors.secondaryBackground,
       }}
     >
-      <View style={{ alignItems: "center", width: "100%" }}>
-        {loadingComplete && (
+      {!loadingComplete ? (
+        <ActivityIndicatorAnimation loadingStatus={!loadingComplete} />
+      ) : (
+        <View style={{ alignItems: "center", width: "100%" }}>
           <PlantOfTheDayCard
             plant={plantsOfTheDay[selectedPlant]}
             key={(item) => item.id + item.scientific_name}
           />
-        )}
-      </View>
-      {(!loadingComplete || loading) && (
-        <ActivityIndicatorAnimation loadingStatus={loading} />
+        </View>
       )}
       <View style={styles.outerCircle}>
         {plantsOfTheDay.length > 0 &&
@@ -125,21 +113,13 @@ const styles = StyleSheet.create({
   },
   innerCircle: {
     width: 100,
-    // height: 100,
     borderRadius: 100,
-    // backgroundColor: "blue",
-    // position: "absolute",
   },
-  child1: {
-    // transform: [{ rotate: "0deg" }],
-    // backgroundColor: "red",
-  },
+  child1: {},
   child2: {
-    // transform: [{ rotate: "120deg" }],
     backgroundColor: "green",
   },
   child3: {
-    // transform: [{ rotate: "240deg" }],
     backgroundColor: "yellow",
   },
 });
